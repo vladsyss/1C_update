829373d8e9637d76ad96736cd05d859f6332dc03
vladsyss <vladsyss@ya.ru>
2025-04-22 13:54:52 +0300

    22.04.2025 13:53
    Добавлен список и обработка этого списка компьютеров не для установки новой версии.

--- 1C_update.ps1
+++ 1C_update.ps1
@@ -4,67 +4,74 @@ $old_ver = '8.3.24.1586'
 $new_ver = '8.3.25.1546'
 $new_ver_64 = '8.3.25.1546_64'
 $listFilePath = "$PSScriptRoot\list.txt"
+$not_install = "pc-051", "pc-052", "pc-069"
 
 function 1C_update {
-	if ($comp.StartsWith("PC")) {
-		if (Test-Connection -count 1 $comp -Quiet) {
-			if (Test-Path "\\$comp\c$\Program Files (x86)") {
-				if (Test-Path "\\$comp\c$\Program Files (x86)\1cv8\$old_ver") {
-					Write-Host $comp - есть старая x86 версия! -ForegroundColor Green
-					$comp + ' - есть старая x86 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
-					if (Test-Path "\\$comp\c$\Program Files (x86)\1cv8\$new_ver") {
-						Write-Host $comp - есть новая x86 версия! -ForegroundColor Green
-						$comp + ' - есть новая x86 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
+	if (-not ($comp -in $not_install)) {
+		if ($comp.StartsWith("PC")) {
+			if (Test-Connection -count 1 $comp -Quiet) {
+				if (Test-Path "\\$comp\c$\Program Files (x86)") {
+					if (Test-Path "\\$comp\c$\Program Files (x86)\1cv8\$old_ver") {
+						Write-Host $comp - есть старая x86 версия! -ForegroundColor Green
+						$comp + ' - есть старая x86 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
+						if (Test-Path "\\$comp\c$\Program Files (x86)\1cv8\$new_ver") {
+							Write-Host $comp - есть новая x86 версия! -ForegroundColor Green
+							$comp + ' - есть новая x86 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
+						}
+						else {
+							Write-Host $comp - Устанавливаю новую x86 версию! -ForegroundColor Green
+							$comp + ' - устанавливаю новую x86 версию!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
+							#& "$PSScriptRoot\PsExec.exe" '-s' '-d' '-n' '60' "\\$comp" "\\srvv-02\NETLOGON\$new_ver\setup.exe" '/S' '/NOW' 2>> "$PSScriptRoot\psexec.log"
+							& $PSScriptRoot\PsExec.exe \\$comp -d -n 60 -s \\srvv-02\NETLOGON\$new_ver\setup.exe /S /NOW 2>> $PSScriptRoot\psexec.log
+							"`n`n===========`n===========" | Out-File "$PSScriptRoot\psexec.log" -Append
+						}
 					}
 					else {
-						Write-Host $comp - Устанавливаю новую x86 версию! -ForegroundColor Green
-						$comp + ' - устанавливаю новую x86 версию!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
-						#& "$PSScriptRoot\PsExec.exe" '-s' '-d' '-n' '60' "\\$comp" "\\srvv-02\NETLOGON\$new_ver\setup.exe" '/S' '/NOW' 2>> "$PSScriptRoot\psexec.log"
-						& $PSScriptRoot\PsExec.exe \\$comp -d -n 60 -s \\srvv-02\NETLOGON\$new_ver\setup.exe /S /NOW 2>> $PSScriptRoot\psexec.log
-						"`n`n===========`n===========" | Out-File "$PSScriptRoot\psexec.log" -Append
+						if (Test-Path "\\$comp\c$\Program Files\1cv8\$old_ver") {
+							Write-Host $comp - есть старая x64 версия! -ForegroundColor Green
+							$comp + ' - есть старая x64 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
+							if (Test-Path "\\$comp\c$\Program Files\1cv8\$new_ver") {
+								Write-Host $comp - есть новая x64 версия! -ForegroundColor Green
+								$comp + ' - есть новая x64 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
+							}
+							else {
+								Write-Host $comp - устанавливаю новую x64 версию! -ForegroundColor Green
+								$comp + ' - устанавливаю новую x64 версию!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
+								#& $PSScriptRoot\PsExec.exe \\$comp -d -n 60 -s \\srvv-01\NETLOGON\$new_ver_64\vc_redist.x64.exe /install /passive /norestart 2>> $PSScriptRoot\psexec.log
+								#Start-Sleep -Seconds 10
+								& $PSScriptRoot\PsExec.exe \\$comp -d -n 60 -s \\srvv-01\NETLOGON\$new_ver_64\setup.exe /S 2>> $PSScriptRoot\psexec.log
+								"`n`n===========`n===========" | Out-File "$PSScriptRoot\psexec.log" -Append
+							}
+						}
 					}
 				}
 				else {
 					if (Test-Path "\\$comp\c$\Program Files\1cv8\$old_ver") {
-						Write-Host $comp - есть старая x64 версия! -ForegroundColor Green
-						$comp + ' - есть старая x64 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
+						Write-Host $comp - есть старая x86 версия! -ForegroundColor Green
+						$comp + ' - есть старая x86 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
 						if (Test-Path "\\$comp\c$\Program Files\1cv8\$new_ver") {
-							Write-Host $comp - есть новая x64 версия! -ForegroundColor Green
-							$comp + ' - есть новая x64 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
+							Write-Host $comp - есть новая x86 версия! -ForegroundColor Green
+							$comp + ' - есть новая x86 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
 						}
 						else {
-							Write-Host $comp - устанавливаю новую x64 версию! -ForegroundColor Green
-							$comp + ' - устанавливаю новую x64 версию!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
-							#& $PSScriptRoot\PsExec.exe \\$comp -d -n 60 -s \\srvv-01\NETLOGON\$new_ver_64\vc_redist.x64.exe /install /passive /norestart 2>> $PSScriptRoot\psexec.log
-							#Start-Sleep -Seconds 10
-							& $PSScriptRoot\PsExec.exe \\$comp -d -n 60 -s \\srvv-01\NETLOGON\$new_ver_64\setup.exe /S 2>> $PSScriptRoot\psexec.log
+							Write-Host $comp - устанавливаю новую x86 версию! -ForegroundColor Green
+							$comp + ' - устанавливаю новую x86 версию!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
+							#& "$PSScriptRoot\PsExec.exe" '-s' '-d' '-n' '60' "\\$comp" "\\srvv-02\NETLOGON\$new_ver\setup.exe" '/S' '/NOW' 2>> "$PSScriptRoot\psexec.log"
+							& $PSScriptRoot\PsExec.exe \\$comp -d -n 60 -s \\srvv-02\NETLOGON\$new_ver\setup.exe /S /NOW 2>> $PSScriptRoot\psexec.log
 							"`n`n===========`n===========" | Out-File "$PSScriptRoot\psexec.log" -Append
 						}
 					}
 				}
 			}
 			else {
-				if (Test-Path "\\$comp\c$\Program Files\1cv8\$old_ver") {
-					Write-Host $comp - есть старая x86 версия! -ForegroundColor Green
-					$comp + ' - есть старая x86 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
-					if (Test-Path "\\$comp\c$\Program Files\1cv8\$new_ver") {
-						Write-Host $comp - есть новая x86 версия! -ForegroundColor Green
-						$comp + ' - есть новая x86 версия!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
-					}
-					else {
-						Write-Host $comp - устанавливаю новую x86 версию! -ForegroundColor Green
-						$comp + ' - устанавливаю новую x86 версию!' | Out-File "$PSScriptRoot\temp.log" -Enc default -Append
-						#& "$PSScriptRoot\PsExec.exe" '-s' '-d' '-n' '60' "\\$comp" "\\srvv-02\NETLOGON\$new_ver\setup.exe" '/S' '/NOW' 2>> "$PSScriptRoot\psexec.log"
-						& $PSScriptRoot\PsExec.exe \\$comp -d -n 60 -s \\srvv-02\NETLOGON\$new_ver\setup.exe /S /NOW 2>> $PSScriptRoot\psexec.log
-						"`n`n===========`n===========" | Out-File "$PSScriptRoot\psexec.log" -Append
-					}
-				}
+				Write-Host $comp - не доступен! -ForegroundColor Red
+				$comp | Out-File $PSScriptRoot\comps_down.txt -Encoding Default -Append
 			}
 		}
-		else {
-			Write-Host $comp - не доступен! -ForegroundColor Red
-			$comp | Out-File $PSScriptRoot\comps_down.txt -Encoding Default -Append
-		}
+	}
+	else {
+		Write-Host $comp - компьютер не для установки новой версии! -ForegroundColor Red
+		$comp | Out-File $PSScriptRoot\comps_down.txt -Encoding Default -Append
 	}
 }
 
